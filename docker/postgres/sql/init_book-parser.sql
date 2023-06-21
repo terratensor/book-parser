b9db708e1fa6a737dbbc83598861d948e4601e84
agusev <audetv@gmail.com>
2023-06-21 23:56:57 +0300

    Изменена схема инициализации БД

--- docker/postgres/sql/init_book-parser.sql
+++ docker/postgres/sql/init_book-parser.sql
@@ -1,6 +1,6 @@
 CREATE TABLE public.books
 (
-    id         bigint   NOT NULL,
+    id         bigint      NOT NULL,
     name       varchar,
     filename   varchar,
     created_at timestamptz NOT NULL,
@@ -9,14 +9,38 @@ CREATE TABLE public.books
 --     CONSTRAINT books_pk PRIMARY KEY (uuid)
 );
 
+CREATE TABLE public.db_pg_books
+(
+    id         bigserial   NOT NULL,
+    name       varchar,
+    filename   varchar,
+    created_at timestamptz NOT NULL,
+    updated_at timestamptz NOT NULL,
+    deleted_at timestamptz NULL,
+    CONSTRAINT db_pg_books_pkey PRIMARY KEY (id)
+);
+
 CREATE TABLE public.book_paragraphs
 (
-    id         bigint         NOT NULL,
-    book_id    bigint         NOT NULL,
+    id         bigint      NOT NULL,
+    book_id    bigint      NOT NULL,
     text       text,
     position   int,
     created_at timestamptz NOT NULL,
     updated_at timestamptz NOT NULL,
     deleted_at timestamptz NULL
 --     CONSTRAINT book_paragraphs_pk PRIMARY KEY (uuid)
+);
+
+CREATE TABLE public.db_pg_paragraphs
+(
+    id         bigserial   NOT NULL,
+    book_id    int8        NOT NULL,
+    text       text,
+    position   int,
+    length     int,
+    created_at timestamptz NOT NULL,
+    updated_at timestamptz NOT NULL,
+    deleted_at timestamptz NULL,
+    CONSTRAINT db_pg_paragraphs_pkey PRIMARY KEY (id)
 );
