5390e653deb255f2b85420e1e7b6ef8d9a013132
agusev <audetv@gmail.com>
2023-07-21 22:40:26 +0300

    Finished concat-splitting paragraphs algorithm, need test

--- common/app/starter/starter.go
+++ common/app/starter/starter.go
@@ -70,8 +70,15 @@ func (app *App) Parse(ctx context.Context, n int, file os.DirEntry, path string)
 	}
 
 	var pars paragraph.PrepareParagraphs
-	var b strings.Builder
-	var bufBuilder strings.Builder
+
+	// var b билдер
+	// var textBuilder билдер для текста прочитанного из docx файла
+	// var bufBuilder промежуточный билдер для текста, для соединения параграфов
+	// var longParBuilder билдер в котором текущий обрабатываемый длинный параграф
+	var b,
+		textBuilder,
+		bufBuilder,
+		longParBuilder strings.Builder
 
 	batchSizeCount := 0
 	for {
@@ -83,130 +90,157 @@ func (app *App) Parse(ctx context.Context, n int, file os.DirEntry, path string)
 		default:
 		}
 
-		text, err := r.Read()
-		if err == io.EOF {
-			break
-		} else if err != nil {
-			str := fmt.Sprintf("%v, %v", filename, err)
-			log.Println(str)
-			return fmt.Errorf(str)
+		// если билдер длинного параграфа пуст и буфер текста пуст,
+		// то читаем следующий параграф из файла docx и передаем его в textBuilder
+		if utf8.RuneCountInString(longParBuilder.String()) == 0 && utf8.RuneCountInString(textBuilder.String()) == 0 {
+			text, err := r.Read()
+			if err == io.EOF {
+				break
+			} else if err != nil {
+				str := fmt.Sprintf("%v, %v", filename, err)
+				log.Println(str)
+				return fmt.Errorf(str)
+			}
+			// Если строка пустая, то пропускаем
+			// и переходим к следующей итерации цикла
+			if text == "" {
+				continue
+			}
+			// Если кол-во символов в тексте больше максимально установленной длины,
+			// записываем текст в буфер большого параграфа, иначе записываем текст в текстовый буфер
+			if utf8.RuneCountInString(text) > app.maxParSize {
+				log.Println("stage 1 — записываем спарсенный текст в буфер большого параграфа,")
+				longParBuilder.WriteString(text)
+			} else {
+				log.Println("stage 2 — записываем спарсенный текст в текстовый буфер")
+				textBuilder.WriteString(text)
+			}
 		}
 
-		// Скорее всего надо сделать здесь ещё цикл и в нем делать проверки
-		// Для того чтобы разбитый параграф возвращал оптимальную часть и остаток,
-		// которые надо проверить, оптимальную часть записать к pars, а остаток проверить на длинный параграф,
-		// и если параграф длинный то в этом цикле крутить пока не обработает весь длинный параграф,
-		// только после этого уже начать обрабатывать текущий text, только после того как предыдущий длинный параграф весь будет разбит
-		// в итерациях этого цикла, обработан и записан в pars
+		log.Printf("longParBuilder.Len() %v", utf8.RuneCountInString(longParBuilder.String()))
+		log.Printf("textBuilder.Len() %v", utf8.RuneCountInString(textBuilder.String()))
+		log.Printf("bBuilder.Len() %v", utf8.RuneCountInString(b.String()))
+		log.Printf("bufBuilder.len() %v", utf8.RuneCountInString(bufBuilder.String()))
 
-		// Если в билдер-буфере есть записанный параграф, то записываем его в обычны билдер b и очищаем билдер-буфер
+		// запись остатка от длинного параграфа в обычный билдер при условии, что остаток менее maxParSize
+		if utf8.RuneCountInString(longParBuilder.String()) > 0 && utf8.RuneCountInString(longParBuilder.String()) < app.maxParSize {
+			//log.Println(utf8.RuneCountInString(b.String()))
+			//log.Println(utf8.RuneCountInString(longParBuilder.String()))
+			log.Println("stage 3 — запись остатка от длинного параграфа в обычный билдер")
+			b.WriteString(longParBuilder.String())
+			longParBuilder.Reset()
+			//panic("exit")
+		}
+		// Если кол-во символов текста в билдере больше максимальной границы maxParSize
+		// разбиваем параграф на 2 части, оптимальной длины и остаток,
+		// остаток сохраняем в longParBuilder, оптимальную часть сохраняем в builder b
+		if utf8.RuneCountInString(longParBuilder.String()) >= app.maxParSize {
+			log.Println("stage 4 — разбиваем параграф на 2 части, оптимальной длины и остаток")
+			app.splitLongParagraph(&longParBuilder, &b)
+		}
+
+		// Если в билдер-буфере есть записанный параграф, то записываем его в обычный билдер b и очищаем билдер-буфер
 		if utf8.RuneCountInString(bufBuilder.String()) > 0 {
-			if utf8.RuneCountInString(bufBuilder.String()) > app.maxParSize {
+			log.Println("stage 5 — записываем bufBuilder в обычный билдер b и очищаем билдер-буфер")
+			if utf8.RuneCountInString(bufBuilder.String()) >= app.maxParSize {
+				log.Println("stage 6")
 				log.Printf("в билдер буфере длинный параграф %v\r\n", utf8.RuneCountInString(bufBuilder.String()))
+				panic("panic")
 			}
 			b.WriteString(bufBuilder.String())
 			bufBuilder.Reset()
 		}
 
-		// Если строка не пустая, то производим обработку
-		if text != "" {
+		// Кол-во символов в билдере, получено от предыдущей итерации
+		builderLength := utf8.RuneCountInString(b.String())
+		//log.Printf("builderLength %v", builderLength)
+		// Кол-во символов в текущем обрабатываемом параграфе, получено из парсера
+		textLength := utf8.RuneCountInString(textBuilder.String())
+		//log.Printf("textLength %v", textLength)
+		// Сумма кол-ва символов в предыдущих склеенных и в текущем параграфах
+		concatLength := builderLength + textLength
+		log.Printf("concatLength %v", concatLength)
 
-			//b.WriteString(text)
-			// Кол-во символов в склеенных параграфах, предыдущей итерации
-			prevLength := utf8.RuneCountInString(b.String())
+		// Если кол-во символов в результирующей строке concatLength менее
+		// минимального значения длины параграфа minParSize,
+		// то соединяем предыдущие параграфы и текущий обрабатываемый,
+		// переходим к следующей итерации цикла и читаем следующий параграф из файла docx,
+		// повторяем пока не достигнем границы минимального значения длины параграфа
 
-			// Кол-во символов в текущем обрабатываемом параграфе
-			curLength := utf8.RuneCountInString(text)
-
-			// Сумма кол-ва символов в предыдущих склеенных и в текущем параграфах
-			concatLength := prevLength + curLength
-			//length := utf8.RuneCountInString(b.String()) + utf8.RuneCountInString(text)
-
-			// Если кол-во символов текста в билдере больше максимальной границы maxParSize
-			if app.minParSize != 0 && prevLength >= app.maxParSize {
-				//log.Printf("cond 1: %v", prevLength)
-				app.doSplitting(&b, &bufBuilder)
-				//continue
-			}
-
-			if app.minParSize != 0 && curLength >= app.maxParSize {
-				//log.Println("cond 2")
-				bufBuilder.WriteString(text)
-				if utf8.RuneCountInString(b.String()) == 0 {
-					continue
-					//panic(b.String())
-				}
-			}
-
-			// Если кол-во символов в результирующей строке concatLength менее
-			// минимального значения длины параграфа minParSize,
-			// то соединяем предыдущие параграфы и текущий обрабатываемый,
-			// переходим к следующей итерации цикла и читаем следующий параграф из файла docx,
-			// повторяем пока не достигнем границы минимального значения длины параграфа
-			if app.minParSize != 0 && concatLength < app.minParSize {
-				//log.Println("cond 3")
-				b.WriteString(text)
-				continue
-			}
-			// Если кол-во символов в результирующей строке билдера более или равно
-			// минимальному значению длины параграфа mixParSize и менее или равно
-			// оптимальному значению длины параграфа, то переходим к следующей итерации цикла
-			// и читаем следующий параграф из файла docx
-			//if app.minParSize != 0 && concatLength >= app.minParSize && concatLength <= app.optParSize {
-			if app.minParSize != 0 && concatLength >= app.minParSize && float64(concatLength) <= float64(app.optParSize)*1.05 {
-				//log.Println("cond 4")
-				// Производим расчет длины текущего параграфа и предыдущих параграфов,
-				// набранных до минимального значения границы длины параграфа minParSize
-				b.WriteString(text)
-				continue
-				//cb.WriteString(b.String())
-				//} else if curLength < app.optParSize {
-			}
-			if app.minParSize != 0 && concatLength > app.optParSize && concatLength <= app.maxParSize {
-				//log.Printf("cond 5 concatLength: %v", concatLength)
-				//log.Printf("cond 5 prevLength: %v", prevLength)
-				//log.Printf("cond 5 curLength: %v", curLength)
-				bufBuilder.WriteString(text)
-
-				if utf8.RuneCountInString(b.String()) == 0 {
-					continue
-					//panic(b.String())
-				}
-			}
-
-			if utf8.RuneCountInString(b.String()) >= app.maxParSize {
-				log.Println(utf8.RuneCountInString(b.String()))
-				panic(b.String())
-			}
-
-			// Записываем текст обработанного параграфа в буфер-билдер
-			//bufBuilder.WriteString(text)
-
-			// Если кол-во символов в результирующей строке билдера более
-			// максимального значения длины параграфа maxParSize,
-			// то вызываем функцию разбивки длинного параграфа DoSplitting
-			//if app.minParSize != 0 && length > app.maxParSize {
-			//	DoSplitting(b.String())
-			//}
-
-			//log.Printf("%v, %v, %v", utf8.RuneCountInString(b.String()) >= app.maxParSize, utf8.RuneCountInString(b.String()), app.maxParSize)
-			pars = appendParagraph(b, newBook, position, pars)
-			b.Reset()
-
-			position++
-			batchSizeCount++
-
-			// Записываем пакетам по batchSize параграфов
-			if batchSizeCount == app.batchSize-1 {
-				err = app.ps.BulkInsert(ctx, pars, len(pars))
-				if err != nil {
-					log.Printf("log bulk insert error query: %v \r\n", err)
-				}
-				// очищаем slice
-				pars = nil
-				batchSizeCount = 0
-			}
+		// и нет длинного параграфа в обработке
+		if concatLength < app.minParSize && utf8.RuneCountInString(longParBuilder.String()) == 0 {
+			log.Println("stage 7")
+			//log.Println("cond 3")
+			b.WriteString(textBuilder.String())
+			textBuilder.Reset()
+			continue
 		}
+		// Если кол-во символов в результирующей строке билдера более или равно
+		// минимальному значению длины параграфа mixParSize и менее или равно
+		// оптимальному значению длины параграфа, то переходим к следующей итерации цикла
+		// и читаем следующий параграф из файла docx
+
+		// и нет длинного параграфа в обработке
+		if concatLength >= app.minParSize &&
+			float64(concatLength) <= float64(app.optParSize)*1.05 &&
+			utf8.RuneCountInString(longParBuilder.String()) == 0 {
+			log.Println("stage 8")
+			// Производим расчет длины текущего параграфа и предыдущих параграфов,
+			// набранных до минимального значения границы длины параграфа minParSize
+			b.WriteString(textBuilder.String())
+			textBuilder.Reset()
+			continue
+		}
+
+		if concatLength > app.optParSize && concatLength <= app.maxParSize {
+			log.Println("stage 9")
+			b.WriteString(textBuilder.String())
+			textBuilder.Reset()
+			//if utf8.RuneCountInString(b.String()) == 0 {
+			//	log.Println("stage 10")
+			//	continue
+			//}
+		}
+
+		if utf8.RuneCountInString(b.String()) >= app.maxParSize {
+			if utf8.RuneCountInString(longParBuilder.String()) == 0 {
+				log.Println("stage 11 — параграф превышает максимальную длину")
+				//longParBuilder.WriteString(b.String())
+				log.Println(b.String())
+				//b.Reset()
+				//panic("exit")
+
+			}
+			//log.Println("stage 12 — параграф превышает максимальную длину")
+			//log.Printf("параграф превышает максимальную длину: %v", utf8.RuneCountInString(b.String()))
+			//log.Printf("параграф превышает максимальную длину: %v", b.String())
+			//log.Printf("longParBuilder.Len() %v", utf8.RuneCountInString(longParBuilder.String()))
+			//log.Printf("textBuilder.Len() %v", utf8.RuneCountInString(textBuilder.String()))
+			//log.Printf("bBuilder.Len() %v", utf8.RuneCountInString(b.String()))
+			//log.Printf("bufBuilder.len() %v", utf8.RuneCountInString(bufBuilder.String()))
+			//panic("exit")
+			//panic(b.String())
+		}
+
+		pars = appendParagraph(b, newBook, position, pars)
+		//textBuilder.Reset()
+		log.Println("stage 100 append")
+		b.Reset()
+
+		position++
+		batchSizeCount++
+
+		// Записываем пакетам по batchSize параграфов
+		if batchSizeCount == app.batchSize-1 {
+			err = app.ps.BulkInsert(ctx, pars, len(pars))
+			if err != nil {
+				log.Printf("log bulk insert error query: %v \r\n", err)
+			}
+			// очищаем slice
+			pars = nil
+			batchSizeCount = 0
+		}
+
 	}
 
 	// Если билдер строки не пустой, записываем оставшийся текст в параграфы и сбрасываем билдер
@@ -225,50 +259,63 @@ func (app *App) Parse(ctx context.Context, n int, file os.DirEntry, path string)
 	return nil
 }
 
-func (app *App) doSplitting(builder *strings.Builder, bufBuilder *strings.Builder) {
-	//log.Printf("Обрабатываем длинный параграф: %v", utf8.RuneCountInString(builder.String()))
-	//log.Printf("длина билдер буфера: %v", utf8.RuneCountInString(bufBuilder.String()))
+func (app *App) splitLongParagraph(longBuilder *strings.Builder, builder *strings.Builder) {
+	log.Printf("Обрабатываем длинный параграф: %v", utf8.RuneCountInString(longBuilder.String()))
+	log.Printf("длина билдер буфера: %v", utf8.RuneCountInString(builder.String()))
 
-	res := builder.String()
-	res = strings.TrimPrefix(res, "<div>")
-	res = strings.TrimSuffix(res, "</div>")
+	result := longBuilder.String()
+	result = strings.TrimPrefix(result, "<div>")
+	result = strings.TrimSuffix(result, "</div>")
 
-	strs := strings.SplitAfter(res, ". ")
+	// sentences []string Делим параграф на предложения
+	sentences := strings.SplitAfter(result, ".")
 
-	builder.Reset()
+	log.Printf("В параграфе %v предложений", len(sentences))
 
-	//var splittedPars []string
-	var buffer strings.Builder
+	longBuilder.Reset()
+	//log.Printf("сброшен longBuilder.String() %v", longBuilder.String())
+
+	//var buffer strings.Builder
 	var flag bool
 
-	for n, str := range strs {
-		//log.Println(str)
+	for n, sentence := range sentences {
+		//log.Printf("предложение длина: %v", utf8.RuneCountInString(sentence))
+		sentence = strings.TrimSpace(sentence)
 		if n == 0 {
-			buffer.WriteString("<div>")
+			builder.WriteString("<div>")
+			//if utf8.RuneCountInString(sentence) > app.optParSize && utf8.RuneCountInString(sentence) < app.minParSize{
+			//	builder.WriteString(sentence)
+			//}
 		}
-		if (utf8.RuneCountInString(buffer.String()) + utf8.RuneCountInString(str) - 4) <= app.optParSize {
-			buffer.WriteString(str)
+		if (utf8.RuneCountInString(builder.String()) + utf8.RuneCountInString(sentence)) < app.optParSize {
+			//log.Printf("sentence %d", n)
+			builder.WriteString(sentence)
+			builder.WriteString(" ")
 			continue
 		}
 		if !flag {
-			//buffer.WriteString("</div>")
-			builder.WriteString(strings.TrimSpace(buffer.String()))
+			//builder.WriteString(strings.TrimSpace(builder.String()))
+			builder.WriteString(strings.TrimSpace(sentence))
 			builder.WriteString("</div>")
-			buffer.Reset()
-			bufBuilder.WriteString("<div>")
 			flag = true
+			if len(sentences) == n+1 {
+				break
+			}
+			longBuilder.WriteString("<div>")
+
+			continue
 		}
 
-		bufBuilder.WriteString(str)
-		//log.Println(builder.String())
-		//panic("done1")
-	}
+		longBuilder.WriteString(sentence)
+		longBuilder.WriteString(" ")
 
-	bufBuilder.WriteString("</div>")
-	//log.Println(builder.String())
-	//log.Println(bufBuilder.String())
-	////builder.Reset()
-	//panic("done2")
+	}
+	if utf8.RuneCountInString(longBuilder.String()) > 0 {
+		temp := longBuilder.String()
+		longBuilder.Reset()
+		longBuilder.WriteString(strings.TrimSpace(temp))
+		longBuilder.WriteString("</div>")
+	}
 }
 
 func appendParagraph(b strings.Builder, newBook *book.Book, position int, pars paragraph.PrepareParagraphs) paragraph.PrepareParagraphs {
